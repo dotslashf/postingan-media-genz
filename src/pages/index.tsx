bf48f80313ce77ec1388a822ab3bacc60ccd8c72
fadhlu <38921923+dotslashf@users.noreply.github.com>
2023-03-18 01:47:05 +0800

    fix h size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="h-screen max-w-lg mx-auto py-4 px-4 shadow-lg flex flex-col space-y-2">
+      <div className="h-full max-w-lg mx-auto py-4 px-4 shadow-lg flex flex-col space-y-2">
         <div>
           <label
             htmlFor="media"
