3ceab2185aa285b1670f7779b79e0d5e403c52a2
fadhlu <38921923+dotslashf@users.noreply.github.com>
2023-03-18 09:58:33 +0800

    supports breakline and various styling

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -9,14 +9,15 @@ interface Props {
     topOffset: number;
     maxWidth?: number;
   };
-  colorMode: string;
+  colorMode: "light" | "dark" | string;
 }
+
+const DARK_COLOR = "#18191A";
+const LIGHT_COLOR = "#FFFFFF";
 
 const Canvas = (props: Props) => {
   const canvas = React.useRef<HTMLCanvasElement>(null);
-  const dark = "#000000";
-  const light = "#ffffff";
-  const textColor = props.colorMode === light ? dark : light;
+  const textColor = props.colorMode === "light" ? DARK_COLOR : LIGHT_COLOR;
   const canvasInfo = {
     width: 500,
     height: 500,
@@ -27,7 +28,7 @@ const Canvas = (props: Props) => {
       const ctx = canvas.current.getContext("2d")!;
       removeMediaText(ctx);
       removeCaptionText(ctx);
-      ctx.fillStyle = props.colorMode;
+      ctx.fillStyle = props.colorMode === "light" ? LIGHT_COLOR : DARK_COLOR;
       ctx.fillRect(0, 0, canvasInfo.width, canvasInfo.height);
       drawMediaText(ctx, props.mediaText);
       drawCaption(ctx, props.caption);
@@ -38,7 +39,7 @@ const Canvas = (props: Props) => {
     props.mediaPosition,
     props.caption,
     props.captionControl,
-    props.colorMode
+    props.colorMode,
   ]);
 
   const removeMediaText = (ctx: CanvasRenderingContext2D) => {
@@ -77,12 +78,11 @@ const Canvas = (props: Props) => {
     if (props.captionControl.align === "right") {
       x = maxWidth;
     }
-    let y = 150;
+    let y = 200;
     if (props.captionControl.topOffset) {
       y += props.captionControl.topOffset;
     }
     wrapText(ctx, text, x, y, maxWidth, lineHeight);
-    // ctx.fillText(text, 250, 250);
   };
 
   const wrapText = (
@@ -93,22 +93,27 @@ const Canvas = (props: Props) => {
     maxWidth: number,
     lineHeight: number
   ) => {
-    const words = text.split(" ");
-    let line = "";
+    const lines = text.split("\n");
 
-    for (let n = 0; n < words.length; n++) {
-      const testLine = line + words[n] + " ";
-      const metrics = ctx.measureText(testLine);
-      const testWidth = metrics.width;
-      if (testWidth > maxWidth && n > 0) {
-        ctx.fillText(line, x, y);
-        line = words[n] + " ";
-        y += lineHeight;
-      } else {
-        line = testLine;
+    for (let i = 0; i < lines.length; i++) {
+      const words = lines[i].split(" ");
+      let line = "";
+
+      for (let n = 0; n < words.length; n++) {
+        let testLine = line + words[n] + " ";
+        const metrics = ctx.measureText(testLine);
+        const testWidth = metrics.width;
+        if (testWidth > maxWidth && n > 0) {
+          ctx.fillText(line, x, y);
+          line = words[n] + " ";
+          y += lineHeight;
+        } else {
+          line = testLine;
+        }
       }
+      ctx.fillText(line, x, y);
+      y += lineHeight;
     }
-    ctx.fillText(line, x, y);
   };
 
   const downloadHandler = () => {
@@ -124,15 +129,30 @@ const Canvas = (props: Props) => {
         ref={canvas}
         width={canvasInfo.width}
         height={canvasInfo.height}
-        className="border border-gray-800 w-full mt-4"
+        className="border border-gray-800 w-full mt-2"
       />
       <div className="mt-4">
         <button
           type="button"
-          className="focus:outline-none text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 "
+          className="focus:outline-none text-white bg-gray-800 hover:bg-gray-900 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 flex items-center shadow-md"
           onClick={downloadHandler}
         >
           Download
+          <svg
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={1.5}
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+            className="ml-4 w-4 h-4"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+            />
+          </svg>
         </button>
       </div>
     </div>
