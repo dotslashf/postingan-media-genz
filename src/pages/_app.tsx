cb4be580a79e658cfdbcd16f83539432d73f6f61
fadhlu <38921923+dotslashf@users.noreply.github.com>
2023-03-19 09:43:30 +0800

    load fonts from next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import { Bebas_Neue, Heebo } from "next/font/google";
+
+export const bebas = Bebas_Neue({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-bebas-neue",
+});
+
+export const heebo = Heebo({
+  variable: "--font-heebo",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <main className={`${bebas.variable} ${heebo.variable}`}>
       <Component {...pageProps} />
       <Analytics />
-    </>
+    </main>
   );
 }
